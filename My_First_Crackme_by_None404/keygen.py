867d88da080a21ee85f7f04c59c939c742de7ac3
Dimi <johnnnathan@proton.me>
2024-10-29 20:08:13 +0100

    Minor alteration

--- My_First_Crackme_by_None404/keygen.py
+++ My_First_Crackme_by_None404/keygen.py
@@ -10,6 +10,8 @@ for char in string:
     final_string += chr(xored_value)
 
 
+# I know that for some reason the 2 turns into a c, I do not know why that happens to it only. Performing the operation manually 
+# using the values inside the cpu registers gives the proper output.
 print("Key : " + str(key))
 print("Pass value: " + final_string)
 print("Should be : flag-216bd5bc-fa07-419a-be80-09464a1c0e97")
