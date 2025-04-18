018585c0df52318a67c25378dfbd85f31d42f0ab
Dimi <johnnnathan@proton.me>
2025-04-18 12:37:31 +0200

    Simple yet sweet

---
+++ third_by_Jenya/keygen.py
@@ -0,0 +1,7 @@
+# The program parses the first character as a number and then compares the following characters against the square of the first. If they are the same a success message is shown. The following code shows all of the correct keys.
+
+# Have a great day :)
+# github.com/johnnnathan
+
+for i in range(1, 10):
+    print("" + str(i) + "" + str(i**2))
